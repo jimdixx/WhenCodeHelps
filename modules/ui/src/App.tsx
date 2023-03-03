005deddded4e4e9731b8b9658c3bd1d0addef50e
Frank Uhal <franquito@Franks-Mac-mini.local>
2023-03-03 22:24:47 +0100

    set up FE for the project

---
+++ modules/ui/src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <div>
+      <h1 className="ext-3xl font-bold underline">Hi</h1>
+      <button className="btn">Button</button>
+    </div>
+  );
+}
+
+export default App
