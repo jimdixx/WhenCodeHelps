b6e73981386a5e07b1def92dc008cd6cd16673f4
Frank Uhal <franquito@Franks-Mac-mini.local>
2023-03-04 08:41:37 +0100

    fix records list

--- modules/ui/src/components/RecordsList.tsx
+++ modules/ui/src/components/RecordsList.tsx
@@ -1,11 +1,24 @@
-import { Player } from "./Player"
+import { useEffect, useState } from "react";
+import { Player } from "./Player";
+import { Recording } from "../types";
 
 export const RecordsList = () => {
-    
-    return (
-      <Player
-        url="https://storage.googleapis.com/media-session/elephants-dream/the-wires.mp3"
-        timestamps={[1.2,2.43,3.55,4.9,6.12,8.24]}
-      />
-    );
-}
+  const [recordings, setRecordings] = useState<Recording[]>([]);
+  useEffect(() => {
+    const records: Recording[] = [
+      {
+        url: "https://storage.googleapis.com/media-session/elephants-dream/the-wires.mp3",
+        timestamps: [1.2, 2.43, 3.55, 4.9, 6.12, 8.24],
+      },
+    ];
+    setRecordings(records);
+  }, []);
+
+  return (
+    <div>
+      {recordings.map((rec, i) => (
+        <Player url={rec.url} timestamps={rec.timestamps} key={i} />
+      ))}
+    </div>
+  );
+};
--- modules/ui/src/types.ts
+++ modules/ui/src/types.ts
@@ -6,7 +6,7 @@ export interface User {
 
 export interface Recording {
     url: string;
-    timestamps: Number[];
+    timestamps: number[];
 }
 
 export interface UserLogin {
