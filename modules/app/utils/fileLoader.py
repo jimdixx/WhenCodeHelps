d6ec51d257174baaf8998c69e2294650856e7760
BerassHaggy <kraus.martin10@gmail.com>
2023-03-04 11:10:00 +0100

    Phonetic transcription added

--- modules/app/utils/fileLoader.py
+++ modules/app/utils/fileLoader.py
@@ -12,4 +12,20 @@ class Loader:
                 split = line.split(" ")
                 for w in split:
                     data.append(w + ".")
+        # fonetic transcription
+        sentence = ""
+        banned = ["v.", "s.", "z.", "v:."]
+        samohlasky = ["a", "e", "i", "o", "u"]
+        for idx, w in enumerate(data):
+            ls = list(w)
+            if not (ls[-1] == "." and ls[-2] == "."):
+                word = data[idx].lower()
+                data[idx] = word
+            sentence += w + " "
+
+        for idx, w in enumerate(data):
+            if w in banned:
+                data[idx + 1] = data[idx].replace(".", "") + "" + data[idx + 1]
+                data.pop(idx)
         return data
+
