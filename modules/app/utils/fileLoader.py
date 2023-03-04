c77d89b92251ecd937c90cf8aebefd224ca7effd
BerassHaggy <kraus.martin10@gmail.com>
2023-03-04 12:04:32 +0100

    Modified phonetic transcription.

--- modules/app/utils/fileLoader.py
+++ modules/app/utils/fileLoader.py
@@ -6,16 +6,28 @@ class Loader:
 
     def loadData(self):
         data = []
+        shorts = {"např.": "například", "Např.": "například", "tzn.": "to znamená",
+                  "Tzn.": "to znamená"}
         with open(self.fName) as f:
             lines = f.readlines()
             for line in lines:
                 split = line.split(" ")
-                for w in split:
-                    data.append(w + ".")
+                for index, w in enumerate(split):
+                    if w in list(shorts.keys()):
+                        w = w.replace(w, shorts[w])
+                        tmp = w.split()
+                        if((isinstance(tmp, list)) and (len(tmp)>1)):
+                            w = w.split()
+                            split.pop(index)
+                            split.insert(index, (w[0] + " " + w[1]))
+                    if((isinstance(w, list)) and (len(w) > 1)):
+                        data.append(w[0] + ". " + w[1] + ".")
+                    else:
+                        data.append(w + ".")
+
         # fonetic transcription
         sentence = ""
         banned = ["v.", "s.", "z.", "v:."]
-        samohlasky = ["a", "e", "i", "o", "u"]
         for idx, w in enumerate(data):
             ls = list(w)
             if not (ls[-1] == "." and ls[-2] == "."):
