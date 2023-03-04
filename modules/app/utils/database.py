22904f80a8610f66fef8d0f74ae4c436125ff62e
Adam <akellich@seznam.cz>
2023-03-04 11:17:55 +0100

    added database template, needs login info now

--- modules/app/utils/database.py
+++ modules/app/utils/database.py
@@ -1,25 +1,52 @@
 import boto3
 
 class Database:
-    ASAK = ""
-    ASKI = ""
-
-    def __init__(self):
-        self.db = boto3
+    def __init__(self, table_name, region_name, aws_access_key_id, aws_secret_access_key):
+        self.table_name = table_name
+        self.region_name = region_name
+        self.aws_access_key_id = aws_access_key_id
+        self.aws_secret_access_key = aws_secret_access_key
         self.client = None
-        self.db = None
+        self.aws_access_key_id = aws_access_key_id
+        self.aws_secret_access_key = aws_secret_access_key
+
+        self.table = None
 
     def connect(self):
         self.client = boto3.client(
             'dynamodb',
-            aws_access_key_id='AKIAZW5Y5IATVYH5VLH7',
-            aws_secret_access_key='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',
-            region="eu-west-1"
+            region_name=self.region_name,
+            aws_access_key_id=self.aws_access_key_id,
+            aws_secret_access_key=self.aws_secret_access_key
         )
-        self.dynamodb = boto3.resource(
+        self.table = boto3.resource(
             'dynamodb',
-            aws_access_key_id='',
-            aws_secret_access_key= '',
-        )
-        ddb_exceptions = self.client.exceptions
+            region_name=self.region_name,
+            aws_access_key_id=self.aws_access_key_id,
+            aws_secret_access_key=self.aws_secret_access_key
+        ).Table(self.table_name)
 
+    def put_item(self, item):
+        try:
+            self.table.put_item(Item=item)
+            return True
+        except self.client.exceptions.ConditionalCheckFailedException as e:
+            print(e)
+            return False
+
+    def get_item(self, key):
+        try:
+            response = self.table.get_item(Key=key)
+            return response.get('Item')
+        except self.client.exceptions.ClientError as e:
+            print(e.response['Error']['Message'])
+            return None
+
+if __name__ == "__main__":
+    db = Database('dynamodb', 'eu-west-1', 'AKIAZW5Y5IATVYH5VLH7', '31rrl9bQQe+aRv6h+4i/NZbVVF2WAcoslgkU08y6')
+    db.connect()
+
+    # Retrieve an item by its primary key
+    key = {'id': 'your_key_value'}
+    item = db.get_item(key)
+    print(item)
