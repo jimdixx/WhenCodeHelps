f5a7243d922eb44d68db823c481c8b25ebae5956
Jan Kuta <jan.kuta@email.cz>
2023-03-04 14:49:24 +0100

    addon pollishing

--- modules/addon/react-chrome-app/src/App.tsx
+++ modules/addon/react-chrome-app/src/App.tsx
@@ -23,21 +23,15 @@ function App() {
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src="/codeHelpsIcon.png" className="logo" alt="Code Helps" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Code Helps</h1>
       <div className="card">
         <p>
           Content: "{content}"
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
