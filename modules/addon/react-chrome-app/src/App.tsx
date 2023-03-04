0a60ddf2d0c61672c52ae038ec889e207e22439d
Jan Kuta <jan.kuta@email.cz>
2023-03-04 18:55:22 +0100

    Addon player

--- modules/addon/react-chrome-app/src/App.tsx
+++ modules/addon/react-chrome-app/src/App.tsx
@@ -1,10 +1,20 @@
 import {useEffect, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import {Player} from "./components/Player";
 
+const simplifyKey = 'simplify'
 function App() {
   const [content, setContent] = useState("")
+  const [simplify, setSimplify] = useState<boolean | undefined>(undefined)
+  const [record, setRecord] = useState<{ url: string, timestamps: number[] } | undefined>({
+    url: "https://storage.googleapis.com/media-session/elephants-dream/the-wires.mp3",
+    timestamps: [1,2,5],
+  })
 
+  useEffect(() => {
+    chrome.storage.local.get().then(res => setSimplify(!!res[simplifyKey]))
+  }, [])
   useEffect(() => {
     chrome.tabs && chrome.tabs.query({
       active: true,
@@ -27,10 +37,17 @@ function App() {
         </a>
       </div>
       <h1>Code Helps</h1>
+      <label>{simplify === undefined ? '?' : <input type="checkbox" checked={simplify} onChange={() => setSimplify(prev => {
+        chrome.storage.local.set({[simplifyKey]: !prev})
+        return !prev
+      })} />} Simplify</label>
       <div className="card">
         <p>
           Content: "{content}"
         </p>
+        {record && (
+          <Player url={record.url} timestamps={record.timestamps} />
+        )}
       </div>
     </div>
   )
