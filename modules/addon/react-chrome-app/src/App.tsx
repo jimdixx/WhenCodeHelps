4e88c12ed34a3d21cfa8ae8a7a67921f793ae4d6
Jan Kuta <jan.kuta@email.cz>
2023-03-05 04:48:43 +0100

    Addon fixed

--- modules/addon/react-chrome-app/src/App.tsx
+++ modules/addon/react-chrome-app/src/App.tsx
@@ -5,21 +5,22 @@ import {API_URL, getAudioTimestamps, getRecord, pushText} from "./api/client";
 
 const simplifyKey = 'simplify'
 function App() {
-  const [content, setContent] = useState("")
+  const [content, setContent] = useState<string | undefined>(undefined)
   const [simplify, setSimplify] = useState<boolean | undefined>(undefined)
   const [timestamps, setTimestamps] = useState<number[] | undefined>(undefined)
   const [cycle, setCycle] = useState(0)
+  const [cycle2, setCycle2] = useState(0)
   const record = `${API_URL}/static/beta.mp3`
   useEffect(() => {
     chrome.storage.local.get().then(res => setSimplify(!!res[simplifyKey]))
   }, [])
 
   useEffect(() => {
-    if (simplify!==undefined){
+    if (simplify!==undefined && content !== undefined){
       pushText(content)
       setCycle(1)
     }
-  }, [simplify])
+  }, [simplify, content])
 
   useEffect(() => {
     if(cycle > 0 && ! timestamps) {
@@ -28,9 +29,11 @@ function App() {
   }, [cycle]);
 
   const fetchAudioTimestamps = useCallback(async () => {
-    return await getAudioTimestamps().then((stamps) =>
-      setTimestamps(stamps)
-    ).catch(() => {setTimeout(() => setCycle(prev => prev + 1), 12000)})
+    return await getAudioTimestamps().then((stamps) => {
+        setTimestamps(stamps)
+        setCycle2(1)
+      }
+    ).catch(() => {setTimeout(() => setCycle(prev => prev + 1), 2000)})
   }, []);
   useEffect(() => {
     chrome.tabs && chrome.tabs.query({
@@ -59,7 +62,7 @@ function App() {
         return !prev
       })} />} Simplify</label>
       <div className="card">
-        {timestamps ? (
+        {timestamps && cycle2 > 0 ? (
           <Player url={record} timestamps={timestamps || []} />
         ) : 'LOADING...'}
         <p>
